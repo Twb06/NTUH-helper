cf8237c2bf1d0efbdc66add0d0d3694458228573
Twb06 <b06401089@g.ntu.edu.tw>
2026-06-02 11:30:19 +0800

    fix: update version to 1.5.0 in user script metadata

--- scripts/NTUH-diagcertificate-filler.user.js
+++ scripts/NTUH-diagcertificate-filler.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         NTUH DiagCertificate & Consent Integrated Filler
 // @namespace    http://tampermonkey.net/
-// @version      1.4.3
+// @version      1.5.0
 // @description  自動填入診斷書，利用背景分頁與 postMessage 跨網域通訊自動擷取手術同意書回傳
 // @author       YT / Twb06
 // @updateURL    https://raw.githubusercontent.com/Twb06/NTUH-helper/main/scripts/NTUH-diagcertificate-filler.user.js
